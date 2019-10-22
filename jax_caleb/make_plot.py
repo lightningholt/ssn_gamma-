102a9fe065edb1ee4851776ae8c3180ef42d4d93
Caleb Holt <cholt@cas.uoregon.edu>
2019-10-22 10:51:33 -0700

    Script to make a bunch of sophisticated plots

---
+++ jax_caleb/make_plot.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+def power_spect_rates_plot(fs, obs_spect, target_spect, contrasts, obs_rates, target_rates, initial_spect=None, initial_rates= None):
+    
+    fig_combined = plt.figure(13, constrained_layout=True)
+    if initial_spect is None:
+        gs = gridspec.GridSpec(4,3, figure=fig_combined)
+    else:
+        gs = gridspec.GridSpec(8, 3, figure=fig_combined)
+
+    ax_spect_targ = fig_combined.add_subplot(gs[0:4,0:2])
+    ax_spect_targ.plot(fs, target_spect, "--")
+    #colrs = ax.get_color_cycle()
+    ax_spect_targ.set_prop_cycle(None)
+    ax_spect_targ.plot(fs, obs_spect)
+    
+    ax_spect_targ.set_title('post gradient descent')
+    ax_spect_targ.set_ylabel('power spectrum (a.u.)')
+    ax_spect_targ.set_xlabel('frequency (Hz)')    
+    
+    if initial_spect is not(None):
+        ax_spect_init = fig_combined.add_subplot(gs[4:,0:2])
+        ax_spect_init.plot(fs, initial_spect, ":")
+        ax_spect_init.set_prop_cycle(None)
+        ax_spect_init.plot(fs, obs_spect)
+        
+        ax_spect_init.set_title('Pre and Post Gradient Descent')
+        ax_spect_init.set_ylabel('power spectrum (a.u.)')
+        ax_spect_init.set_xlabel('frequency (Hz)')   
+        
+    
+    ax_E = fig_combined.add_subplot(gs[0:2, 2])
+    ax_I = fig_combined.add_subplot(gs[2:4, 2])
+
+    ax_E.plot(contrasts, target_rates[:, 0],  "r--")
+    ax_E.set_prop_cycle(None)
+    ax_E.plot(contrasts, obs_rates[:,0], 'r')
+    ax_E.set_title('E Rates Post GD')
+    # ax_E.set_xlabel('Contrasts')
+    # ax_E.set_ylabel('Firing Rates (Hz)')
+
+    ax_I.plot(contrasts, target_rates[:, 1],  "b--")
+    ax_I.set_prop_cycle(None)
+    ax_I.plot(contrasts, obs_rates[:,1], 'b')
+    ax_I.set_title('I Rates Post GD')
+    
+    if initial_rates is not(None):
+        ax_E_init = fig_combined.add_subplot(gs[4:6, 2])
+        ax_I_init = fig_combined.add_subplot(gs[6:8, 2])
+        
+        ax_E_init.plot(contrasts, obs_rates[:,0], "r")
+        ax_E_init.set_prop_cycle(None)
+        ax_E_init.plot(contrasts, initial_rates[:,0], "r:")
+        
+        ax_I_init.plot(contrasts, obs_rates[:,1], "b")
+        ax_I_init.set_prop_cycle(None)
+        ax_I_init.plot(contrasts, initial_rates[:,1], "b:")
+        
+        ax_E_init.set_title('E Rates Pre/Post')
+        ax_I_init.set_title('I Rates Pre/Post')
+    
+    # ax_I.set_xlabel('Contrasts')
+    # ax_I.set_ylabel('Firing Rates (Hz)')
