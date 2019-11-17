49749117a910a2536aa33a8273f5ff4c31382c09
Caleb <cholt@uoregon.edu>
2019-11-17 09:27:16 -0800

    added in differences in PS and not fitting BS

--- jax_caleb/ssn_gamma_2D.py
+++ jax_caleb/ssn_gamma_2D.py
@@ -197,8 +197,8 @@ def loss(params):
     fs_loss_inds = np.array([freq for freq in fs_loss_inds if fs[freq] >20])#np.where(fs > 0, fs_loss_inds, )
 #     fs_loss = fs[np.where(fs > 20)]
     
-    spect_loss = losses.loss_spect_contrasts(fs[fs_loss_inds], np.real(spect[fs_loss_inds, :]))
-    #$spect_loss = losses.loss_spect_nonzero_contrasts(fs[fs_loss_inds], spect[fs_loss_inds,:])
+    #spect_loss = losses.loss_spect_contrasts(fs[fs_loss_inds], np.real(spect[fs_loss_inds, :]))
+    spect_loss = losses.loss_spect_nonzero_contrasts(fs[fs_loss_inds], spect[fs_loss_inds,:])
     rates_loss = prefact_rates * losses.loss_rates_contrasts(r_fp[:,1:], lower_bound_rates, upper_bound_rates, kink_control) #fourth arg is slope which is set to 1 normally
     #rates_loss = prefact_rates * losses.loss_rates_contrasts(r_fp, lower_bound_rates, upper_bound_rates, kink_control) # recreate ground truth
     param_loss = prefact_params * losses.loss_params(params)
