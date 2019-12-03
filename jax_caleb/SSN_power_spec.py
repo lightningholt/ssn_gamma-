896f57d9b5eb4be9b89fc8dd490b71873d151585
Caleb <cholt@uoregon.edu>
2019-12-03 14:16:52 -0800

    now works for the multi-probe case

--- jax_caleb/SSN_power_spec.py
+++ jax_caleb/SSN_power_spec.py
@@ -8,6 +8,8 @@ from util import toeplitz
 
 class NoisePars(object):
 	def __init__(self, stdevE=1.5, stdevI=1.5, corr_time=1, corr_length=0, NMDAratio=0):
+        #NMDAratio is Inp.NoiseNMDAratio = 0 in the MATLAB code
+        # in other words, its how much NMDA contributes to noise in network, not the actual amount of NMDA receptors in the network
 		self.stdevE = stdevE
 		self.stdevI = stdevI
 		self.corr_time = corr_time
