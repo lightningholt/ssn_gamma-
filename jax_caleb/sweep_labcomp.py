9982a1ce8d4bd2fba87d7d56792004e3444bddcb
Caleb <cholt@uoregon.edu>
2020-02-16 21:04:08 -0800

    doing a search for the rescaled modelos

--- jax_caleb/sweep_labcomp.py
+++ jax_caleb/sweep_labcomp.py
@@ -6,18 +6,29 @@ import scipy.io as sio
 
 aa = sio.loadmat('hand_selected_ConEffect_1_23.mat')
 
+psi = 0.774 *np.pi
+g0=0.44
+Wconv = 1
+
+W0 = 1.8
+Jxe_min = W0*.5
+Jxe_max = W0*1.5 - Jxe_min
+Jxi_min = W0*.5 * .5
+Jxi_max = W0*1.5 * .5 - Jxi_min
+
+
 # extract all parameters as 1-dim arrays
 Plocal = aa['Params']['Plocal'][0,0][0]
-Jee = aa['Params']['Jee'][0, 0][:,0]
-Jei = np.abs(aa['Params']['Jei'][0, 0][:,0]) #MATLAB had these defined as negative
-Jie = aa['Params']['Jie'][0, 0][:,0]
-Jii = np.abs(aa['Params']['Jii'][0, 0][:,0]) #MATLAB had these defined as negative
+Jee = np.maximum(aa['Params']['Jee'][0, 0][:,0], Jxe_min * psi)
+Jei = np.maximum(np.abs(aa['Params']['Jei'][0, 0][:,0]), Jxi_min * psi) #MATLAB had these defined as negative
+Jie = np.maximum(aa['Params']['Jie'][0, 0][:,0], Jxe_min * psi)
+Jii = np.maximum(np.abs(aa['Params']['Jii'][0, 0][:,0]), Jxi_min * psi) #MATLAB had these defined as negative
 I2E = aa['Params']['I2E'][0,0][0]
 sigEE = aa['Params']['sigEE'][0,0][:,0]
 sigIE = aa['Params']['sigIE'][0,0][:,0]
 
 #hyper Params
-diffPS = True
+diffPS = False
 ground_truth = False
 OLDSTYLE = False
 lamSS = 10
@@ -46,10 +57,10 @@ min_loss = 100
 min_loss_ind = 0
 
 for rgi in real_good_inds:
-    fname = 'matlab_'+str(rgi)+'_diffPS_'+dps+'_GT_'+gt+'_SI_'+si+'_lamSS_'+str(lamSS)+'.pdf'
+    fname = 'newRange_'+str(rgi)+'_diffPS_'+dps+'_GT_'+gt+'_SI_'+si+'_lamSS_'+str(lamSS)+'.pdf'
     hyper_params = {'diffPS':diffPS, 'ground_truth':ground_truth, 'OLDSTYLE':OLDSTYLE, 'SI':SI, 'fname':fname, 'lamSS':lamSS}
 
-    params_init = np.array([Jee[rgi]/psi, Jei[rgi]/psi, Jie[rgi]/psi, Jii[rgi]/psi, 1, I2E[rgi], 0.1, Plocal[rgi], Plocal[rgi], sigEE[rgi], sigIE[rgi]])
+    params_init = np.array([Jee[rgi]/psi, Jei[rgi]/psi, Jie[rgi]/psi, Jii[rgi]/psi, g0, g0*I2E[rgi], 0.1, Plocal[rgi], Plocal[rgi], sigEE[rgi], sigIE[rgi]])
     params_init = find_params_to_sigmoid(params_init, MULTI=True, OLDSTYLE=OLDSTYLE)
 
     _, _, _, loss_t = ssn_multi_probes.bfgs_multi_gamma(params_init, hyper_params)
