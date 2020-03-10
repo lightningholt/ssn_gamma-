8fdabb40fd1aa639d23e1fa6bb96cdcb6f31c628
Caleb Holt <cholt@cas.uoregon.edu>
2020-03-10 06:57:59 -0700

    not sure what I changed here

--- ssn-multi-NoJax/MakeSSNconnectivity.py
+++ ssn-multi-NoJax/MakeSSNconnectivity.py
@@ -111,7 +111,7 @@ def make_Wxx_dist(dist, ori_dist, sigma, sigma_ori, from_neuron, MinSyn=1e-4, JN
     
     return W
 
-def make_full_W(Plocal, Jee, Jei, Jie, Jii, sigEE, sigIE, deltaD, OMap, sigXI = 0.02, PlocalIE = None):
+def make_full_W(Plocal, Jee, Jei, Jie, Jii, sigEE, sigIE, deltaD, OMap, sigXI = 0.09, PlocalIE = None):
     '''
     Function that makes the full rank W = [[Wee, Wei], [Wie, Wii]] which obeys Dale's law (meaning Wxi is defined negative)
     
@@ -192,6 +192,7 @@ def makeInputs(OMap, r_cent, contrasts, X, Y, gridsizedeg=4, gridperdeg=5, AngWi
     
     RFdecay = 0.8/2 #biologic decay is 0.8 mm, magfactor =2 mm/deg 
     RFdecay = 0.1 * RFdecay
+    RFdecay = 0.04
     #RFdecay = dx
     #GaborSigma = 0.3*np.max(r_cent) 
     GaborSigma = 0.5
